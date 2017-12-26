674ec87e6531ad7bd56ceda090295602a0377e32
wossl33 <34831078+wossl33@users.noreply.github.com>
2017-12-26 18:03:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from csr import csr
 from rsa import rsa
 from des import des
 from hashs import hashs
+from feis import feis
+
 app=Flask(__name__)
 
 # 加载配置文件
@@ -16,3 +18,4 @@ app.register_blueprint(csr,url_prefix='/csr')
 app.register_blueprint(rsa,url_prefix='/rsa')
 app.register_blueprint(des,url_prefix='/des')
 app.register_blueprint(hashs,url_prefix='/hashs')
+app.register_blueprint(feis,url_prefix='/feis')
