e4f35797b81917a215f95f68a12ae7c4498c1420
wossl33 <wossl33@163.com>
2018-01-03 15:31:53 +0800

    update feis

--- feis/base.py
+++ feis/base.py
@@ -18,7 +18,6 @@ def pubkey_tiqu(key_pem_data,key_passwd=None):
     try:
         key=serialization.load_pem_private_key(key_pem_data,password=key_passwd,backend=default_backend())
     except Exception,e:
-        print e
         return {'error':False,'msg':u'私钥解析失败！'}
     public_key_pem=key.public_key().public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
     public_key_der=key.public_key().public_bytes(serialization.Encoding.DER,serialization.PublicFormat.SubjectPublicKeyInfo)
@@ -30,8 +29,7 @@ def pubkey_asysi(public_pem_data):
     rep_reuslt={}
     try:
         public_key=serialization.load_pem_public_key(public_pem_data,backend=default_backend())
-    except Exception,e:
-        print e
+    except Exception,e:        
         return {'error':False,'msg':u'公钥解析失败！'}
     # 密钥类型
     if isinstance(public_key,rsa.RSAPublicKey):
@@ -50,3 +48,128 @@ def pubkey_asysi(public_pem_data):
     rep_reuslt['public_key_size']=public_key.key_size
     rep_reuslt['public_key_der']=b2a_hex(public_key.public_bytes(serialization.Encoding.DER,serialization.PublicFormat.SubjectPublicKeyInfo))
     return {'error':True,'msg':rep_reuslt}
+
+# 公私钥校验
+def pub_priv_checker(pub_pem_data,key_pem_data,pass_key=None):
+    try:
+        pub_key=serialization.load_pem_public_key(pub_pem_data,backend=default_backend())
+    except Exception,e:
+        return {'error':False,'msg':u'公钥内容错误！'}
+
+    try:
+        priv_key=serialization.load_pem_private_key(key_pem_data,password=pass_key,backend=default_backend())
+    except Exception,e:
+        return {'error':False,'msg':u'私钥内容或KEY错误！'}
+    public_key=pub_key.public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
+    key_public_key=priv_key.public_key().public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
+    if public_key == key_public_key:
+        return {'error':True,'msg':u'匹配成功！'}
+    else:
+        return {'error':False,'msg':u'匹配失败！'}
+
+# 公私钥对生成
+def pub_priv_creater(mysf,myqd,key_pass):
+    if mysf == 'RSA':
+        private_key=rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=int(myqd),
+            backend=default_backend()
+        )
+        if key_pass:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.BestAvailableEncryption(key_pass),
+            )
+        else:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        public_key=private_key.public_key().public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
+        return {'error':True,'pub_key':public_key,'priv_key':key}
+    elif mysf == 'DSA':
+        private_key=dsa.generate_private_key(
+            key_size=int(myqd),
+            backend=default_backend()
+        )
+        if key_pass:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.BestAvailableEncryption(key_pass),
+            )
+        else:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        public_key=private_key.public_key().public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
+        return {'error':True,'pub_key':public_key,'priv_key':key}
+    elif mysf =='ECDSA':
+        if myqd == 'P192':
+            private_key=ec.generate_private_key(
+                curve=ec.SECP192R1(),
+                backend=default_backend()
+            )
+        elif myqd == 'P224':
+            private_key=ec.generate_private_key(
+                curve=ec.SECP224R1(),
+                backend=default_backend()
+            )
+        elif myqd == 'P256':
+            private_key=ec.generate_private_key(
+                curve=ec.SECP256R1(),
+                backend=default_backend()
+            )
+        elif myqd == 'P384':
+            private_key=ec.generate_private_key(
+                curve=ec.SECP384R1(),
+                backend=default_backend()
+            )
+        elif myqd == 'P521':
+            private_key=ec.generate_private_key(
+                curve=ec.SECP521R1(),
+                backend=default_backend()
+            )
+        else:
+            private_key=ec.generate_private_key(
+                curve=ec.SECP256R1(),
+                backend=default_backend()
+            )
+
+        if key_pass:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.BestAvailableEncryption(key_pass),
+            )
+        else:
+            key=private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        public_key=private_key.public_key().public_bytes(serialization.Encoding.PEM,serialization.PublicFormat.SubjectPublicKeyInfo)
+        return {'error':True,'pub_key':public_key,'priv_key':key}
+    else:
+        return {'error':False,'pub_key':u'选择加密算法错误！'}
+
+# 私钥密码修改
+def private_xiugai(key_pem_data,old_key,new_key):
+    try:
+        priv_key=serialization.load_pem_private_key(key_pem_data,password=old_key,backend=default_backend())
+    except Exception,e:
+        return {'error':False,'msg':u'私钥内容或原密码错误！'}
+
+    try:
+        new_priv_key=priv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption(new_key),
+        )
+    except Exception,e:
+        return {'error':False,'msg':u'新密码格式错误！'}
+    return {'error':True,'new_key':new_priv_key,'new_pass':new_key}
--- feis/views.py
+++ feis/views.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from app import feis
-from base import pubkey_tiqu,pubkey_asysi
+from base import pubkey_tiqu,pubkey_asysi,pub_priv_checker,pub_priv_creater,private_xiugai
 from flask import request,render_template
 
 # 公钥提取
@@ -16,6 +16,17 @@ def tiqu_hander():
         return render_template('feis/tiqu_result.html',tiqu_result=pubkey_tiqu(key_content,key))
     else:
         return render_template('feis/tiqu_result.html',tiqu_result=pubkey_tiqu(key_content))
+
+@feis.route('/web_pub_get',methods=['POST'])
+def web_pub_get():
+    key_content=request.form['key_content'].encode('utf-8')
+    key=str(request.form['key'])
+    if key:
+        return render_template('web/public_get_result.html',tiqu_result=pubkey_tiqu(key_content,key))
+    else:
+        return render_template('web/public_get_result.html',tiqu_result=pubkey_tiqu(key_content))
+
+
 # 公钥解析
 @feis.route('/pub_asysi')
 def pub_asysi():
@@ -25,11 +36,84 @@ def pub_asysi():
 def asysi_hander():
     public_key_content=request.form['public_key_content'].encode('utf-8')
     return render_template('feis/asysi_result.html',rep_result=pubkey_asysi(public_key_content))
-# 公钥加解密
-@feis.route('/pub_jjm')
-def pub_jjm():
-    return render_template('feis/jiajiemi.html')
+
+@feis.route('/web_pub_asysi',methods=['POST'])
+def web_pub_asysi():
+    public_key_content=request.form['public_key_content'].encode('utf-8')
+    return render_template('web/public_asysi_result.html',rep_result=pubkey_asysi(public_key_content))
+
 # 公私钥校验
 @feis.route('/pub_priv')
 def pub_priv():
     return render_template('feis/jiaoyan.html')
+
+# 公私钥校验处理
+@feis.route('/pub_priv_hander',methods=['POST'])
+def pub_priv_hander():
+    public_key=request.form['public_key'].encode('utf-8')
+    private_key=request.form['private_key'].encode('utf-8')
+    key=str(request.form['key'])
+    if key:
+        return render_template('feis/jiaoyan_result.html',jy_result=pub_priv_checker(public_key,private_key,key))
+    else:
+        return render_template('feis/jiaoyan_result.html',jy_result=pub_priv_checker(public_key,private_key))
+
+# 公私钥校验处理
+@feis.route('/web_pub_priv_hander',methods=['POST'])
+def web_pub_priv_hander():
+    public_key=request.form['public_key'].encode('utf-8')
+    private_key=request.form['private_key'].encode('utf-8')
+    key=str(request.form['key'])
+    if key:
+        return render_template('web/pub_priv_jy_result.html',jy_result=pub_priv_checker(public_key,private_key,key))
+    else:
+        return render_template('web/pub_priv_jy_result.html',jy_result=pub_priv_checker(public_key,private_key))    
+
+# 密钥对生成
+@feis.route('/pub_key_create')
+def pub_key_create():
+    return render_template('feis/pub_priv_double.html')
+
+@feis.route('/pub_priv_create_hander',methods=['POST'])
+def pub_priv_create_hander():
+    mysf=str(request.form['mysf'])
+    if str(request.form['myqd']):
+        myqd=str(request.form['myqd'])
+    elif mysf == 'ECDSA' and not str(request.form['myqd']):
+        myqd='P256'
+    else:
+        myqd='2048'
+    key_pass=str(request.form['key_pass'])
+    return render_template('feis/pub_priv_double_result.html',result=pub_priv_creater(mysf,myqd,key_pass))
+
+
+@feis.route('/web_pass_hander',methods=['POST'])
+def web_pass_hander():
+    mysf=str(request.form['mysf'])
+    if str(request.form['myqd']):
+        myqd=str(request.form['myqd'])
+    elif mysf == 'ECDSA' and not str(request.form['myqd']):
+        myqd='P256'
+    else:
+        myqd='2048'
+    key_pass=str(request.form['key_pass'])
+    return render_template('web/pass_double_result.html',result=pub_priv_creater(mysf,myqd,key_pass))
+
+# 私钥密码修改
+@feis.route('/priv_xiugai')
+def priv_xiugai():
+    return render_template('feis/priv_xiugai.html')
+
+@feis.route('/priv_xg_hander',methods=['POST'])
+def priv_xg_hander():
+    private_key=request.form['priv_content'].encode('utf-8')
+    old_key=str(request.form['old_key'])
+    new_key=str(request.form['new_key'])
+    return render_template('feis/priv_xiugai_result.html',key_reuslt=private_xiugai(private_key,old_key,new_key))
+
+@feis.route('/web_priv_xg',methods=['POST'])
+def web_priv_xg():
+    private_key=request.form['priv_content'].encode('utf-8')
+    old_key=str(request.form['old_key'])
+    new_key=str(request.form['new_key'])
+    return render_template('web/priv_xg_result.html',key_reuslt=private_xiugai(private_key,old_key,new_key))
